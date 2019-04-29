c797a28dcec607c40b86517d2aa525c1e0171094
guoshen <guo.shen@husky.neu.edu>
2019-04-29 11:48:46 -0700

    add video item

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,3 +1,13 @@
 import React from 'react';
+import { removePropertiesDeep } from '@babel/types';
+
+const VideoItem = ({video}) => {
+    return (
+        <div>
+            <img src={video.snippet.thumbnails.medium.url} />
+            {video.snippet.title}
+        </div>
+    )
+}
 
 export default VideoItem;
