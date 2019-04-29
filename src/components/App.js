ba618b5f0dbbf52429f9e8cf75077c5e8587f3eb
guoshen <guo.shen@husky.neu.edu>
2019-04-29 13:04:10 -0700

    add callback function to notify App which video is clicked

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import youtube from '../apis/youtube';
+import VideoDetail from './VideoDetail';
 
 
 class App extends React.Component{
-    state = {videos: []};
+    state = {videos: [], selectedVideo: null};
     onSearchSubmit(term){
         youtube.get('./search', {
             params: {
@@ -15,14 +16,18 @@ class App extends React.Component{
             this.setState({
                 videos: response.data.items
             }));
-        
+    }
+
+    onVideoSelect(video){
+        this.setState({selectedVideo: video});
     }
 
     render(){
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={term => this.onSearchSubmit(term)}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail selectedVideo={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={video => this.onVideoSelect(video)} videos={this.state.videos}/>
             </div>
         );
     }
