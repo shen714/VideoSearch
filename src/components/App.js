d9368c4aca201bf72a6fd3e9741d9c430a8740b4
guoshen <guo.shen@husky.neu.edu>
2019-04-29 11:49:53 -0700

    add callback functon

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import Youtube from '../apis/youtube';
+import VideoList from './VideoList';
 import youtube from '../apis/youtube';
 
 
@@ -11,13 +11,18 @@ class App extends React.Component{
             params: {
                 q: term
             }
-        });
+        }).then(response => 
+            this.setState({
+                videos: response.data.items
+            }));
+        
     }
 
     render(){
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={term => this.onSearchSubmit(term)}/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
