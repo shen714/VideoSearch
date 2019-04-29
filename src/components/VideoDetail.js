cfa342588bb39b8f949afafb27754f002b9bb6f0
guoshen <guo.shen@husky.neu.edu>
2019-04-29 13:04:36 -0700

    to show the detal of movie

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,3 +1,16 @@
 import React from 'react';
 
+const VideoDetail = ({selectedVideo}) => {
+    if(! selectedVideo){
+        return <div>loading...</div>
+    }
+
+    return (
+      <div className="ui segament">
+        <h4 className="ui header">{selectedVideo.snippet.title}</h4>
+        <p>{selectedVideo.snippet.description}</p>
+     </div>
+    )
+}
+
 export default VideoDetail;
