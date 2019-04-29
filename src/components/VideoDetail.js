12222932b8d4fc72e0834d92135b81b690909ff8
guoshen <guo.shen@husky.neu.edu>
2019-04-29 13:36:48 -0700

    show the video play on the screen

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,11 +5,19 @@ const VideoDetail = ({selectedVideo}) => {
         return <div>loading...</div>
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${selectedVideo.id.videoId}`
+
     return (
-      <div className="ui segament">
-        <h4 className="ui header">{selectedVideo.snippet.title}</h4>
-        <p>{selectedVideo.snippet.description}</p>
-     </div>
+        <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
+
+            <div className="ui segment">
+                <h4 className="ui header">{selectedVideo.snippet.title}</h4>
+                <p>{selectedVideo.snippet.description}</p>
+            </div>
+        </div>
     )
 }
 
